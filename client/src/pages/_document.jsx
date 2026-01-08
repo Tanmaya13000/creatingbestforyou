0891439db7d58003f7a6b4931b1867ce4e87d5ce
Your Name <tanmayasantoshsurve786@gmail.com>
2026-01-08 20:20:56 +0530

    Added Some Animations

--- client/src/pages/_document.jsx
+++ client/src/pages/_document.jsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/Images/OurLogo/ourlogobg.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
