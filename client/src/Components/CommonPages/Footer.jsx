f6c326e7bea868fc16b29c217785fcbd78c50d65
Your Name <tanmayasantoshsurve786@gmail.com>
2026-01-09 13:16:36 +0530

    added link

--- client/src/Components/CommonPages/Footer.jsx
+++ client/src/Components/CommonPages/Footer.jsx
@@ -136,7 +136,7 @@ const Footer = () => {
               <div className="group leading-[7vw] sm:leading-[5.5vw] md:leading-[4.5vw] lg:leading-[3.5vw] xl:leading-[2.5vw] [&:hover>a:not(:hover)]:opacity-30 ">
                 {[
                   { name: "LinkedIn", link: "https://www.linkedin.com/in/creatingbestforyou" },
-                  { name: "Instagram", link: "https://www.instagram.com" },
+                  { name: "Instagram", link: "https://www.instagram.com/creatingbestforyou/" },
                 ].map((item) => (
                   <Copy>
                     <a
