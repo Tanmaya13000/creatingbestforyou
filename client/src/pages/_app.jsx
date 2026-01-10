dad02940b4da907713098fb0abb83dfe9f334846
Your Name <tanmayasantoshsurve786@gmail.com>
2026-01-10 12:37:41 +0530

    add google analytics

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -85,7 +85,7 @@ export default function App({ Component, pageProps }) {
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-S77ECMESJQ"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
 
         gtag('config', 'G-S77ECMESJQ');
